f5542901f6c61e3a77ba28ca8052d0e74cba3df0
desc_inno <descinno@gmail.com>
2020-05-23 23:22:23 +0800

    Update steam-reviews-filter.user.js

--- steam-reviews-filter.user.js
+++ steam-reviews-filter.user.js
@@ -3,14 +3,12 @@
 // @description  remove some reviews contains key words
 // @author       desc_inno
 // @namespace    https://github.com/desc70865/steam-reviews-filter
-// @updateURL    https://github.com/desc70865/steam-reviews-filter/issues
+// @supportURL   https://github.com/desc70865/steam-reviews-filter/issues
 // @updateURL    https://github.com/desc70865/steam-reviews-filter/steam-reviews-filter.user.js
 // @version      0.1
 // @icon         https://store.steampowered.com/favicon.ico
 // @match        https://steamcommunity.com/app/*/reviews/*
 // @match        https://store.steampowered.com/app/*
-// @require      https://ajax.googleapis.com/ajax/libs/prototype/1.7.1.0/prototype.js
-// @require      https://steamcommunity-a.akamaihd.net/public/javascript/apphub_home.js
 // @grant        none
 // ==/UserScript==
 
@@ -26,7 +24,7 @@
 
 function remove(domain){
     var reviews;
-    var reg_filter = /我是傻逼/g; // 在此设置屏蔽关键字,多个用|隔开,形如: /我是傻A|我是傻B|我是傻C/g
+    var reg_filter = /portal|PORTAL|Portal|传送门/g; // 在此设置屏蔽关键字,多个用|隔开,形如: /我是傻A|我是傻B|我是傻C/g
     if(domain == "steamcommunity.com"){
         reviews = document.querySelectorAll('.apphub_CardTextContent');
     }else{
