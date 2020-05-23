ef6d89052bb8c2a7010e5aa09146591d284224f3
desc_inno <descinno@gmail.com>
2020-05-23 23:58:55 +0800

    Update steam-reviews-filter.user.js

--- steam-reviews-filter.user.js
+++ steam-reviews-filter.user.js
@@ -1,31 +1,32 @@
 // ==UserScript==
-// @name         steam reviews filter
-// @description  remove some reviews contains key words
+// @name         Steam Reviews Filter
+// @description  remove some reviews contain(s) key words
 // @author       desc_inno
 // @namespace    https://github.com/desc70865/steam-reviews-filter
 // @supportURL   https://github.com/desc70865/steam-reviews-filter/issues
 // @updateURL    https://github.com/desc70865/steam-reviews-filter/steam-reviews-filter.user.js
-// @version      0.1
+// @version      0.2
 // @icon         https://store.steampowered.com/favicon.ico
 // @match        https://steamcommunity.com/app/*/reviews/*
 // @match        https://store.steampowered.com/app/*
+// @match        https://store.steampowered.com/
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    var domain = location.href.split('/')[2];
-    if(domain == "steamcommunity.com"){
+    var flag = location.href.split('/')[2] == "steamcommunity.com";
+    if(flag){
         window.setInterval(CheckForMoreContent, 3000);
     };
-    remove(domain);
-    window.setInterval(function(){ remove(domain); }, 3000); // 可按需设置刷新间隔
+    remove(flag);
+    window.setInterval(function(){ remove(flag); }, 3000); // 可按需设置刷新间隔
 
 })();
 
-function remove(domain){
+function remove(flag){
     var reviews;
     var reg_filter = /我是傻逼/g; // 在此设置屏蔽关键字,多个用|隔开,形如: /我是傻A|我是傻B|我是傻C/g
-    if(domain == "steamcommunity.com"){
+    if(flag){
         reviews = document.querySelectorAll('.apphub_CardTextContent');
     }else{
         reviews = document.querySelectorAll('.content');
